f20824a0bc2a738abac3191bed32a8d2a3dcac9e
Simao <bonvalots@gmail.com>
2023-08-29 15:30:55 +0100

    refactored settings file into a folder with a file for each environment

---
+++ authors_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [("Simao Bonvalot", "smmbonvalot@gmail.com")]
+
+#TODO add domain names of the production servers
+CSRF_TRUSTED_ORIGINS = [""]
